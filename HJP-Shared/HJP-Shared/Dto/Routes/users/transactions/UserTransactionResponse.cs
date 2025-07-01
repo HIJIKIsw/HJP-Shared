b4b2109b71396d2ae38ecdb4992bfb8880309d84
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2025-07-01 19:07:59 +0900

    UserTransactionレスポンス用のコンテナクラスを別ファイルに切り離した

--- HJP-Shared/HJP-Shared/Dto/Routes/users/transactions/UserTransactionResponse.cs
+++ HJP-Shared/HJP-Shared/Dto/Routes/users/transactions/UserTransactionResponse.cs
@@ -1,21 +1,7 @@
-using Hjp.Shared.Entities;
-
 namespace Hjp.Shared.Dto
 {
     public class UserTransactionResponse
     {
-        public class Container
-        {
-            public required int TransactionTypeId { get; set; }
-            public string? FromUserName { get; set; }
-            public ulong? FromDiscordUserId { get; set; }
-            public string? ToUserName { get; set; }
-            public ulong? ToDiscordUserId { get; set; }
-            public required int Amount { get; set; }
-            public string? Description { get; set; }
-            public required DateTime DateTime { get; set; }
-        }
-
-        public required List<UserTransactionResponse.Container> Transactions { get; set; }
+        public required List<UserTransactionResponseItem> Transactions { get; set; }
     }
 }
---
+++ HJP-Shared/HJP-Shared/Dto/Routes/users/transactions/UserTransactionResponseItem.cs
@@ -0,0 +1,14 @@
+namespace Hjp.Shared.Dto
+{
+    public class UserTransactionResponseItem
+    {
+        public required int TransactionTypeId { get; set; }
+        public required string? FromUserName { get; set; }
+        public required ulong? FromDiscordUserId { get; set; }
+        public required string? ToUserName { get; set; }
+        public required ulong? ToDiscordUserId { get; set; }
+        public required int Amount { get; set; }
+        public required string? Description { get; set; }
+        public required DateTime DateTime { get; set; }
+    }
+}
