f3e6287666346c6fed896c3f6a5125216bf18485
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2025-07-04 00:38:32 +0900

    入金/出金レスポンスDTOにDiscordUserIdをセットするプロパティを追加

--- HJP-Shared/Dto/Routes/users/deposit/UserDepositResponse.cs
+++ HJP-Shared/Dto/Routes/users/deposit/UserDepositResponse.cs
@@ -2,6 +2,7 @@ namespace Hjp.Shared.Dto.Users.Deposit
 {
     public class UserDepositResponse
     {
+        public required ulong DiscordUserId { get; set; }
         public required int BeforeAmount { get; set; }
         public required int AfterAmount { get; set; }
     }
--- HJP-Shared/Dto/Routes/users/withdraw/UserWithdrawResponse.cs
+++ HJP-Shared/Dto/Routes/users/withdraw/UserWithdrawResponse.cs
@@ -2,6 +2,7 @@ namespace Hjp.Shared.Dto.Users.Withdraw
 {
     public class UserWithdrawResponse
     {
+        public required ulong DiscordUserId { get; set; }
         public required int BeforeAmount { get; set; }
         public required int AfterAmount { get; set; }
     }
