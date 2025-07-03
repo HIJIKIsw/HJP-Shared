e97f2919e9705cdac2429624b05549a2c75d7d7c
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2025-07-04 02:15:52 +0900

    バージョンAPI用のDTOを追加

---
+++ HJP-Shared/Dto/Routes/version/VersionResponse.cs
@@ -0,0 +1,28 @@
+namespace Hjp.Shared.Dto.Routes.version
+{
+    public class VersionResponse
+    {
+        /// <summary>
+        /// メジャーバージョン
+        /// </summary>
+        /// <remarks>
+        /// API構造が根本的に変わるなど、大規模なアップデート時にインクリメントされる。
+        /// このバージョンが一致する限り、破壊的変更は行われない。
+        /// </remarks>
+        public required int Major { get; set; }
+        /// <summary>
+        /// マイナーバージョン
+        /// </summary>
+        /// <remarks>
+        /// 主にAPI追加時など、破壊的変更でないアップデート時にインクリメントされる。
+        /// </remarks>
+        public required int Minor { get; set; }
+        /// <summary>
+        /// パッチバージョン
+        /// </summary>
+        /// <remarks>
+        /// 主にバグ修正やレスポンスプロパティ追加など、軽微なアップデート時にインクリメントされる。
+        /// </remarks>
+        public required int Patch { get; set; }
+    }
+}
