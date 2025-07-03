19f732bc225f5cb3a2253bf488bb13e3c873080f
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2025-07-04 01:40:54 +0900

    ユーザ名とDiscordUserIdは指定しなくても検索可能にした

--- HJP-Shared/Dto/Routes/admin/users/search/AdminUserSearchRequest.cs
+++ HJP-Shared/Dto/Routes/admin/users/search/AdminUserSearchRequest.cs
@@ -3,9 +3,9 @@ namespace Hjp.Shared.Dto.Admin.Users.Search
 {
     public class AdminUserSearchRequest
     {
-        public string UserName { get; set; } = null!;
-        public bool IsPartialMatchUserName { get; set; } = false;
-        public ulong DiscordUserId { get; set; }
-        public bool IsPartialMatchDiscordUserId { get; set; } = false;
+        public string? UserName { get; set; }
+        public bool IsPartialMatchUserName { get; set; }
+        public ulong? DiscordUserId { get; set; }
+        public bool IsPartialMatchDiscordUserId { get; set; }
     }
 }
