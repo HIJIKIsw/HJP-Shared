0cb3f330749cd3f0b82daddee95fd3d84011dcdf
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2025-07-04 00:21:31 +0900

    管理者権限ユーザ取得レスポンスに情報を追加
    - 所持金
    - パーミッションタイプ

--- HJP-Shared/Dto/Routes/admin/users/AdminUserResponse.cs
+++ HJP-Shared/Dto/Routes/admin/users/AdminUserResponse.cs
@@ -1,8 +1,11 @@
 using Hjp.Shared.Dto.Users.Me;
+using Hjp.Shared.Enums;
 
 namespace Hjp.Shared.Dto.Admin.Users
 {
     public class AdminUserResponse : UserResponse
     {
+        public required int CurrencyAmount { get; set; }
+        public required PermissionType PermissionTypeId { get; set; }
     }
 }
