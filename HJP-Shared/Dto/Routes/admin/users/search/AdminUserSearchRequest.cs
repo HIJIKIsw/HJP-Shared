e7aaab6d3084c04fd2883056388d5dd04e260c65
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2025-07-04 01:05:50 +0900

    検索ユーザ条件を指定しないこともできるようにした

--- HJP-Shared/Dto/Routes/admin/users/search/AdminUserSearchRequest.cs
+++ HJP-Shared/Dto/Routes/admin/users/search/AdminUserSearchRequest.cs
@@ -3,9 +3,9 @@ namespace Hjp.Shared.Dto.Admin.Users.Search
 {
     public class AdminUserSearchRequest
     {
-        public required string UserName { get; set; }
-        public required bool IsPartialMatchUserName { get; set; } = false;
-        public required ulong DiscordUserId { get; set; }
-        public required bool IsPartialMatchDiscordUserId { get; set; } = false;
+        public required string? UserName { get; set; }
+        public required bool? IsPartialMatchUserName { get; set; } = false;
+        public required ulong? DiscordUserId { get; set; }
+        public required bool? IsPartialMatchDiscordUserId { get; set; } = false;
     }
 }
