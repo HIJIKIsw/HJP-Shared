28c3a8e2533a5e53c6855859a72c0671fddf614c
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2025-07-04 00:47:56 +0900

    ユーザ送金のレスポンスDTOに送金先のDiscordUserIdをセットするプロパティを追加

--- HJP-Shared/Dto/Routes/users/transfer/UserTransferResponse.cs
+++ HJP-Shared/Dto/Routes/users/transfer/UserTransferResponse.cs
@@ -2,6 +2,7 @@ namespace Hjp.Shared.Dto.Users.Transfer
 {
     public class UserTransferResponse
     {
+        public required ulong ToDiscordUserId { get; set; }
         public required int BeforeAmount { get; set; }
         public required int AfterAmount { get; set; }
     }
