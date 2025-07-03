e634f95ab247c6aff364a6ea7c1580c8aa9d3ea6
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2025-07-03 23:41:49 +0900

    アクセストークン生成時にユーザ情報を更新するためのDTOを追加

---
+++ HJP-Shared/Dto/Routes/moderator/users/access-token/reset/ModeratorUserAccessTokenRequest.cs
@@ -0,0 +1,8 @@
+namespace Hjp.Shared.Dto.Moderator.Users.AccessToken.Reset
+{
+    public class ModeratorUserAccessTokenResetRequest
+    {
+        public required string UserName { get; set; }
+        public required string AvatarUrl { get; set; }
+    }
+}
