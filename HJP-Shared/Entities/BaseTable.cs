81c4c7155339c4a5209ae6d09cacdea2dd812905
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2025-07-04 23:03:49 +0900

    楽観ロック用のカラムを追加

--- HJP-Shared/Entities/BaseTable.cs
+++ HJP-Shared/Entities/BaseTable.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Hjp.Shared.Entities
 {
     public abstract class BaseTable
     {
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; } = null!;
     }
 }
