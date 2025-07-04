60a27be9d97edb2344bcb898ba9c661d602eb553
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2025-07-05 02:43:06 +0900

    くじを引いたときのレスポンスに積立金を返すプロパティを追加

--- HJP-Shared/Dto/Routes/users/me/lottery/UserLotteryResponse.cs
+++ HJP-Shared/Dto/Routes/users/me/lottery/UserLotteryResponse.cs
@@ -5,6 +5,7 @@ namespace Hjp.Shared.Dto.Routes.Users.Me.Lottery
         public required bool IsWin { get; set; }
         public required int BeforeAmount { get; set; }
         public required int AfterAmount { get; set; }
+        public required int BankAmount { get; set; }
         public required string Message { get; set; }
     }
 }
