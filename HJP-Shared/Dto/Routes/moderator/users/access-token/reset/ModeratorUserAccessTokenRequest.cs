61665b7ddf1e302fce662eaf7112286a23a7b208
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2025-07-06 16:01:39 +0900

    アクセストークン再発行時のリクエストプロパティをnullableにした

--- HJP-Shared/Dto/Routes/moderator/users/access-token/reset/ModeratorUserAccessTokenRequest.cs
+++ HJP-Shared/Dto/Routes/moderator/users/access-token/reset/ModeratorUserAccessTokenRequest.cs
@@ -2,7 +2,7 @@ namespace Hjp.Shared.Dto.Moderator.Users.AccessToken.Reset
 {
     public class ModeratorUserAccessTokenResetRequest
     {
-        public required string UserName { get; set; }
-        public required string AvatarUrl { get; set; }
+        public required string? UserName { get; set; }
+        public required string? AvatarUrl { get; set; }
     }
 }
