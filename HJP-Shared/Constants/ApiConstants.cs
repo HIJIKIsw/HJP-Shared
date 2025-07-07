a09d0190ae9a56590a5f872547e43b9f9dc053b4
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2025-07-07 20:43:20 +0900

    ClaimTypes定数を追加

--- HJP-Shared/Constants/ApiConstants.cs
+++ HJP-Shared/Constants/ApiConstants.cs
@@ -1,7 +1,15 @@
 namespace Hjp.Shared.Constants
 {
-    public class ApiConstants
+    public static class ApiConstants
     {
+        public static class ClaimTypes
+        {
+            private const string Prefix = "Hjp.Api.";
 
+            public const string UserName = Prefix + "UserName";
+            public const string DiscordUserId = Prefix + "DiscordUserId";
+            public const string AvatarUrl = Prefix + "AvatarUrl";
+            public const string PermissionType = Prefix + "PermissionType";
+        }
     }
 }
