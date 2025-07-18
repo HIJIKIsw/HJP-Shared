b81aab3e0275b6dd7d336be83c52f2216e7757de
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2025-07-18 18:33:26 +0900

    トランザクション履歴APIのレスポンスにユーザのアバター画像URLプロパティを追加

--- HJP-Shared/Dto/Routes/admin/transactions/AdminTransactionsResponseItem.cs
+++ HJP-Shared/Dto/Routes/admin/transactions/AdminTransactionsResponseItem.cs
@@ -5,8 +5,10 @@ namespace Hjp.Shared.Dto.Admin.Transactions
         public required int TransactionTypeId { get; set; }
         public required string? FromUserName { get; set; }
         public required ulong? FromDiscordUserId { get; set; }
+        public required string? FromUserAvatarUrl { get; set; }
         public required string? ToUserName { get; set; }
         public required ulong? ToDiscordUserId { get; set; }
+        public required string? ToUserAvatarUrl { get; set; }
         public required int Amount { get; set; }
         public required int? ToUserBalanceAfter { get; set; }
         public required int? FromUserBalanceAfter { get; set; }
--- HJP-Shared/Dto/Routes/users/me/transactions/UserTransactionsResponseItem.cs
+++ HJP-Shared/Dto/Routes/users/me/transactions/UserTransactionsResponseItem.cs
@@ -5,8 +5,10 @@ namespace Hjp.Shared.Dto.Users.Me.Transactions
         public required int TransactionTypeId { get; set; }
         public required string? FromUserName { get; set; }
         public required ulong? FromDiscordUserId { get; set; }
+        public required string? FromUserAvatarUrl { get; set; }
         public required string? ToUserName { get; set; }
         public required ulong? ToDiscordUserId { get; set; }
+        public required string? ToUserAvatarUrl { get; set; }
         public required int Amount { get; set; }
         public required int BalanceAfter { get; set; }
         public required string? Description { get; set; }
