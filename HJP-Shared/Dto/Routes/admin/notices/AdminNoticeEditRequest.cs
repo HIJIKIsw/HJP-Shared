c93a9f70b894fa58facc2fae4e5f5cd7a198f397
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2025-07-19 21:36:31 +0900

    削除/編集対象はルートで指定するため、IDは不要だった

--- HJP-Shared/Dto/Routes/admin/notices/AdminNoticeEditRequest.cs
+++ HJP-Shared/Dto/Routes/admin/notices/AdminNoticeEditRequest.cs
@@ -2,7 +2,6 @@ namespace Hjp.Shared.Dto.Admin.Notices
 {
     public class AdminNoticesEditRequest : AdminNoticePostRequest
     {
-        public required int Id { get; set; }
         public required bool IsRemoved { get; set; }
     }
 }
--- HJP-Shared/Dto/Routes/admin/notices/AdminNoticeRemoveRequest.cs
+++
@@ -1,7 +0,0 @@
-namespace Hjp.Shared.Dto.Admin.Notices
-{
-    public class AdminNoticeRemoveRequest
-    {
-        public required int Id { get; set; }
-    }
-}
