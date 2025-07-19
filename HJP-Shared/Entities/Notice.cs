80cd9ee5f100db1e9abd12f54448a7ab71581cd6
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2025-07-19 22:06:49 +0900

    お知らせタイトルの文字数制限を追加

--- HJP-Shared/Entities/Notice.cs
+++ HJP-Shared/Entities/Notice.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Hjp.Shared.Entities
 {
     public class Notice : BaseTable
     {
         public int Id { get; set; }
+        [StringLength(128)]
         public required string Title { get; set; } = default!;
         public required string Body { get; set; } = default!;
         public DateTime? PublishedAt { get; set; }
