3f0a5b7a7baf4478da6e160bf03009eb014fb5a3
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2025-07-19 20:12:02 +0900

    クラス名の誤りを修正

--- HJP-Shared/Dto/Routes/admin/notices/AdminNoticesRequest.cs
+++ HJP-Shared/Dto/Routes/admin/notices/AdminNoticesRequest.cs
@@ -2,7 +2,7 @@ using Hjp.Shared.Dto.Notices;
 
 namespace Hjp.Shared.Dto.Admin.Notices
 {
-    public class AdminNoticeRequest : NoticesRequest
+    public class AdminNoticesRequest : NoticesRequest
     {
         // Nothing to do.
     }
