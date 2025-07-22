c95213f532f83d84d91fe13d8ceb33402f713559
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2025-07-22 19:42:36 +0900

    ログイン時に権限タイプを指定しないようにした

--- HJP-Shared/Dto/Routes/auth/LoginRequest.cs
+++ HJP-Shared/Dto/Routes/auth/LoginRequest.cs
@@ -1,10 +1,7 @@
-using Hjp.Shared.Enums;
-
 namespace Hjp.Shared.Dto.Auth
 {
     public class LoginRequest
     {
         public required string AccessToken { get; set; }
-        public PermissionType? AsPermissionTypeId { get; set; }
     }
 }
