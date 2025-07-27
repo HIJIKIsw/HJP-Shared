521c7bf10a8d0984371d1fe5e5da62660ee9775b
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2025-07-27 19:24:04 +0900

    タイムゾーンはTimeZoneInfo型ではなくIdをstringで渡す必要があるため修正

--- HJP-Shared/Dto/Routes/me/balance/history/UserBalanceHistoryRequest.cs
+++ HJP-Shared/Dto/Routes/me/balance/history/UserBalanceHistoryRequest.cs
@@ -5,13 +5,13 @@ namespace Hjp.Shared.Dto.Me.Balance.History
     public class UserBalanceHistoryRequest
     {
         /// <summary>
-        /// タイムゾーン
+        /// タイムゾーンID
         /// </summary>
         /// <remarks>
         /// 日次の取引履歴を取得する際に使用するタイムゾーン。
         /// レスポンスで返る日時はUTCである点に注意。
         /// </remarks>
-        public required TimeZoneInfo TimeZone { get; set; }
+        public required string TimeZoneId { get; set; } = "Asia/Tokyo";
         /// <summary>
         /// 取得開始日
         /// </summary>
