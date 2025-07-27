b8b167c9cfb89e8e618084adabbd2a4540c0363b
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2025-07-27 22:19:49 +0900

    取引が発生するAPIのリクエストで、外部アプリのIDを渡すパラメータを追加

---
+++ HJP-Shared/Dto/Routes/me/lottery/UserLotteryRequest.cs
@@ -0,0 +1,7 @@
+namespace Hjp.Shared.Dto.Me.Lottery
+{
+    public class UserLotteryRequest
+    {
+        public required int ApplicationId { get; set; }
+    }
+}
--- HJP-Shared/Dto/Routes/me/withdraw/UserWithdrawRequest.cs
+++ HJP-Shared/Dto/Routes/me/withdraw/UserWithdrawRequest.cs
@@ -2,6 +2,7 @@ namespace Hjp.Shared.Dto.Me.Withdraw
 {
     public class UserWithdrawRequest
     {
+        public required int ApplicationId { get; set; }
         public required int Amount { get; set; }
         public string? Description { get; set; }
     }
