8911a8b703c31aade89ec16d52b8415987f0c851
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2025-07-27 23:37:01 +0900

    Reapply "取引が発生するAPIのリクエストで、外部アプリのIDを渡すパラメータを追加"
    This reverts commit e0e0029a4935a1c18bde51767e1290dc990d5da0.

--- HJP-Shared/Dto/Routes/me/deposit/UserDepositRequest.cs
+++ HJP-Shared/Dto/Routes/me/deposit/UserDepositRequest.cs
@@ -2,6 +2,7 @@ namespace Hjp.Shared.Dto.Me.Deposit
 {
     public class UserDepositRequest
     {
+        public required int ApplicationId { get; set; }
         public required int Amount { get; set; }
         public string? Description { get; set; }
     }
--- HJP-Shared/Dto/Routes/me/transfer/UserTransferRequest.cs
+++ HJP-Shared/Dto/Routes/me/transfer/UserTransferRequest.cs
@@ -2,6 +2,7 @@ namespace Hjp.Shared.Dto.Me.Transfer
 {
     public class UserTransferRequest
     {
+        public required int ApplicationId { get; set; }
         public required ulong ToDiscordUserId { get; set; }
         public required int Amount { get; set; }
         public string? Description { get; set; }
