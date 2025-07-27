2a3c5ca82ca533d794193748c3d8cd362182971b
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2025-07-27 21:58:55 +0900

    外部アプリを登録するためのテーブル定義を追加

---
+++ HJP-Shared/Entities/IntegrationApplication.cs
@@ -0,0 +1,10 @@
+namespace Hjp.Shared.Entities
+{
+    public class IntegrationApplication : BaseTable
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required int ApplicationId { get; set; }
+        public required string? IconUrl { get; set; }
+    }
+}
