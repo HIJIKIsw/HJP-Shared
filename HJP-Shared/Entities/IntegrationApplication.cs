e5e068d5c3a9a9ed0345937ebd6f65706e5a365a
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2025-07-27 23:06:52 +0900

    連携アプリの名称に長さ制限を追加

--- HJP-Shared/Entities/IntegrationApplication.cs
+++ HJP-Shared/Entities/IntegrationApplication.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Hjp.Shared.Entities
 {
     public class IntegrationApplication : BaseTable
     {
         public int Id { get; set; }
+        [StringLength(128)]
         public required string Name { get; set; }
         public required int ApplicationId { get; set; }
         public required string? IconUrl { get; set; }
