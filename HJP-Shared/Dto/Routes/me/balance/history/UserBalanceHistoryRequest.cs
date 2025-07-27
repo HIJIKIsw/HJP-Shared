e8240b3d07e599ebefd263d6ee775ccc3d64b054
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2025-07-27 19:20:27 +0900

    注意コメント追記

--- HJP-Shared/Dto/Routes/me/balance/history/UserBalanceHistoryRequest.cs
+++ HJP-Shared/Dto/Routes/me/balance/history/UserBalanceHistoryRequest.cs
@@ -5,8 +5,12 @@ namespace Hjp.Shared.Dto.Me.Balance.History
     public class UserBalanceHistoryRequest
     {
         /// <summary>
-        /// タイムゾーン
+        /// タイムゾーン
         /// </summary>
+        /// <remarks>
+        /// 日次の取引履歴を取得する際に使用するタイムゾーン。
+        /// レスポンスで返る日時はUTCである点に注意。
+        /// </remarks>
         public required TimeZoneInfo TimeZone { get; set; }
         /// <summary>
         /// 取得開始日
