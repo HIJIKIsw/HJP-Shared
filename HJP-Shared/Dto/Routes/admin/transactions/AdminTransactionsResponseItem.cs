0d3b5e4e6c42cd2f497bc330599b6a9dd8f7fa9c
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2025-07-28 19:27:30 +0900

    取引履歴レスポンスに連携アプリ情報をセットするプロパティを追加

--- HJP-Shared/Dto/Routes/admin/transactions/AdminTransactionsResponseItem.cs
+++ HJP-Shared/Dto/Routes/admin/transactions/AdminTransactionsResponseItem.cs
@@ -3,6 +3,9 @@ namespace Hjp.Shared.Dto.Admin.Transactions
     public class AdminTransactionsResponseItem
     {
         public required int TransactionTypeId { get; set; }
+        public required string IntegrationApplicationName { get; set; }
+        public required int IntegrationApplicationId { get; set; }
+        public required string IntegrationApplicationIconUrl { get; set; }
         public required string? FromUserName { get; set; }
         public required ulong? FromDiscordUserId { get; set; }
         public required string? FromUserAvatarUrl { get; set; }
